35aa2f533479fd403675c0014d144a6140b35074
Luis Torres <luisgabrieltorresvidal@gmail.com>
2022-11-03 11:38:14 -0500

    solucion de errores2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceRootUrl(Config::get('app.url'));    
-        if (str_contains(Config::get('app.url'), 'https://')) {
-            URL::forceScheme('https');
-        }
     }
 }
